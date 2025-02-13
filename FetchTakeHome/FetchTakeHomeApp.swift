ec233942f221903961dd032c454d51df91b7385f
Sako Hovaguimian <sakohovaguimian@yahoo.com>
2025-02-13 11:18:57 -0800

    Initial Commit

---
+++ FetchTakeHome/FetchTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTakeHomeApp.swift
+//  FetchTakeHome
+//
+//  Created by Sako Hovaguimian on 2/13/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
